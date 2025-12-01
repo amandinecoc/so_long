faebcb8a9fa326f36cdf818103b40a5c64128e51
Amandine Cocoual <acocoual@f2r6s20.clusters.42paris.fr>
2025-12-01 19:57:51 +0100

    1.0.0

--- so_long.h
+++ so_long.h
@@ -6,7 +6,50 @@
 /*   By: acocoual <acocoual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 19:46:35 by acocoual          #+#    #+#             */
-/*   Updated: 2025/12/01 19:46:35 by acocoual         ###   ########.fr       */
+/*   Updated: 2025/12/01 19:56:05 by acocoual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+/* ************************************************************************** */
+/*INCLUDES*/
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+#include <errno.h> 
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+#include <sys/stat.h>
+# include <unistd.h>
+
+/* ************************************************************************** */
+/*TYPEDEF*/
+// typedef enum e_status
+// {
+// 	Success,
+// 	malloc_failure,
+// 	command_not_found,
+// }			t_status;
+
+// typedef struct s_pipex
+// {
+// 	char	*file1;
+// 	char	*cmd1;
+// 	char	*file2;
+// 	char	*cmd2;
+// 	char	**tab_cmd1;
+// 	char	**tab_cmd2;
+// 	char	**tab_path;
+// }			t_pipex;
+
+/* ************************************************************************** */
+/*PARSING*/
+
+/* ************************************************************************** */
+/*SO_LONG*/
+
+
+#endif
